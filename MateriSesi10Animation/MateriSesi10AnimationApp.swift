786013bc20a0a9c1e91f68fb878e5c8d6deae5f4
naelafauzul <naelafauzulm@gmail.com>
2024-02-06 17:08:28 +0700

    Initial Commit

---
+++ MateriSesi10Animation/MateriSesi10AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MateriSesi10AnimationApp.swift
+//  MateriSesi10Animation
+//
+//  Created by Naela Fauzul Muna on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MateriSesi10AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
